4a26e69aab9912a30706ce8a1d26b44608dbef04
georgep973 <peralta.george973@gmail.com>
2024-01-25 16:52:32 -0500

    Added stretch features

--- app/src/main/java/com/example/simplecounter/MainActivity.kt
+++ app/src/main/java/com/example/simplecounter/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.simplecounter
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -16,10 +17,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val button = findViewById<Button>(R.id.buttonCounterAdd)
         val textView = findViewById<TextView>(R.id.counterNum)
+        val upgradeButton = findViewById<Button>(R.id.upgradeButton)
         button.setOnClickListener {
             //Toast.makeText(it.context, "Clicked Button!", Toast.LENGTH_SHORT).show()
             counter++
             textView.text = counter.toString()
+
+            if (counter >= 100){
+                upgradeButton.visibility = View.VISIBLE
+                upgradeButton.setOnClickListener {
+                    button.text = "Add 2"
+
+                    button.setOnClickListener {
+                        counter+=2
+                        textView.text = counter.toString()
+                    }
+                    upgradeButton.visibility = View.INVISIBLE
+                }
+            }
         }
     }
 }
